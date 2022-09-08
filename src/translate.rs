73acfeb862ddcfc8a75e80828094fce6d3948f09
YotioSoft <ytani0323@gmail.com>
2022-09-09 00:31:28 +0900

    complete!

--- src/translate.rs
+++ src/translate.rs
@@ -6,12 +6,14 @@ use curl::easy::Easy;
 
 /// 翻訳
 pub fn translate(auth_key:String, text: String, target_lang: String, source_lang: String) -> String {
-    let d = format!("https://api-free.deepl.com/v2/translate?auth_key={}&text={}&target_lang={}&source_lang={}", auth_key, text, target_lang, source_lang);
+    let d = format!("auth_key={}&text={}&target_lang={}&source_lang={}", auth_key, text, target_lang, source_lang);
     let mut dst = Vec::new();
     println!("{}", d);
 
     let mut easy = Easy::new();
-    easy.url(d.as_str()).unwrap();
+    easy.url("https://api-free.deepl.com/v2/translate").unwrap();
+    easy.post(true).unwrap();
+    easy.post_fields_copy(d.as_bytes()).unwrap();
     {
         let mut transfer = easy.transfer();
         transfer.write_function(|data| {
