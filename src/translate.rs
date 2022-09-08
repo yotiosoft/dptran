bc70e85501575ae9212f368970a3ee67803368d3
YotioSoft <ytani0323@gmail.com>
2022-09-09 00:26:52 +0900

    send a word

--- src/translate.rs
+++ src/translate.rs
@@ -1,21 +1,25 @@
 //! 入力文章の原文の送信と翻訳文の受信
 
+use std::io::{stdout, Write};
+use std::str;
 use curl::easy::Easy;
 
 /// 翻訳
 pub fn translate(auth_key:String, text: String, target_lang: String, source_lang: String) -> String {
-    let data = format!("auth_key={}&text={}&target_lang={}&source_lang={}", auth_key, text, target_lang, source_lang).as_bytes();
+    let d = format!("https://api-free.deepl.com/v2/translate?auth_key={}&text={}&target_lang={}&source_lang={}", auth_key, text, target_lang, source_lang);
+    let mut dst = Vec::new();
+    println!("{}", d);
 
     let mut easy = Easy::new();
-    easy.url("https://api-free.deepl.com/v2/translate").unwrap();
-    easy.post(true).unwrap();
-    easy.post_field_size(data.len() as u64).unwrap();
+    easy.url(d.as_str()).unwrap();
+    {
+        let mut transfer = easy.transfer();
+        transfer.write_function(|data| {
+            dst.extend_from_slice(data);
+            Ok(data.len())
+        }).unwrap();
+        transfer.perform().unwrap();
+    }
 
-    let mut transfer = easy.transfer();
-    transfer.read_function(|buf| {
-        Ok(data.as_bytes().read(buf).unwrap_or(0))
-    }).unwrap();
-    transfer.perform().unwrap();
-
-    "あいうえお".to_string()
+    str::from_utf8(&dst).unwrap().to_string()
 }
