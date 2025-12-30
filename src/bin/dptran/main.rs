25cf00f445b7a40466ab2a5f86066dff9c0f2c4c
yotiosoft <yotio@yotiosoft.com>
2025-12-30 23:22:07 +0900

    bin/main: fix for runtime_glossary_tests

--- src/bin/dptran/main.rs
+++ src/bin/dptran/main.rs
@@ -1732,7 +1732,7 @@ mod runtime_tests {
         // Remove glossary (if exists)
         let mut cmd = Command::new("cargo");
         std::thread::sleep(std::time::Duration::from_secs(2));
-        let text = cmd.arg("run")
+        let _ = cmd.arg("run")
             .arg("--release")
             .arg("--")
             .arg("glossary")
@@ -1740,11 +1740,6 @@ mod runtime_tests {
             .arg("--name")
             .arg("test_glossary_id")
             .output();
-        assert!(text.is_ok());
-        let text = text.unwrap();
-        if text.status.success() != true {
-            panic!("Error: {}", String::from_utf8_lossy(&text.stderr));
-        }
 
         // Create glossary
         let mut cmd = Command::new("cargo");
